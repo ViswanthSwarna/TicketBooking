d01062a4b35e5cab51321891088b78a0a7eb1364
93RakeshS <r.salunkhe@devon.nl>
2023-04-06 09:47:37 +0530

    added test cases for TicketBookingController and CityService GetAllCities method

--- TicketBookingBackend/TicketBooking.Test/ServiceTest/TicketBookingServiceTest.cs
+++ TicketBookingBackend/TicketBooking.Test/ServiceTest/TicketBookingServiceTest.cs
@@ -1,9 +1,29 @@
+using Moq;
+using TicketBooking.Repository.Interfaces;
+using TicketBooking.Services.Classes;
+using TicketBooking.Services.Interfaces;
+
 namespace TicketBooking.Test.ServiceTest
 {
     public class TicketBookingServiceTest
     {
+        private readonly Mock<ITicketRepository> _repository;
+
+        private readonly ITicketService _ticketService;
+        public TicketBookingServiceTest()
+        {
+            _repository = new Mock<ITicketRepository>();
+            _ticketService = new TicketService(_repository.Object);
+        }
+
         [Fact]
-        public void Test1()
+        public void SaveTicket_Returns_True() 
+        { 
+        
+        }
+
+        [Fact]
+        public void SaveTicket_Returns_False()
         {
 
         }
